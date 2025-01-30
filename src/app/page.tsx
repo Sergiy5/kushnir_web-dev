dbe1365b31740270e1391adb8e3a2620f1cd1256
Serhii Kushnir <s1983kushnir@gmail.com>
2025-01-30 12:52:08 +0100

    Remove trash from page and added sprite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 import { Hero } from "./components/Hero";
 import { ProjectsList } from "./components/ProjectsList";
 
-// import Image from "next/image";
-{
-  /* <Image
-  className="dark:invert"
-  src=""
-  alt=""
-  width={180}
-  height={38}
-  priority
-/>; */
-}
 export default function Home() {
   return (
-    <div className="flex-col items-center justify-between gap-16 pt-16 pb-36">
+    <div className="flex flex-col items-center justify-between gap-[144px] pt-16 pb-36">
       <Hero />
       <ProjectsList />
     </div>
