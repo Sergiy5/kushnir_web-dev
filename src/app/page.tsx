47ac0744674549706fea0558317ceb9bc92baed5
Serhii Kushnir <s1983kushnir@gmail.com>
2025-01-31 20:29:52 +0100

    Uncomit Projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { ClockContactsSection } from "./components/ClockContactSection";
-// import { ProjectsList } from "./components/ProjectsList";
+import { ProjectsList } from "./components/ProjectsList";
 import { Hero } from "./components/Hero";
 import { ServicesList } from "./components/ServicesList";
 
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between gap-[144px] pt-16 pb-36">
       <Hero />
-      {/* <ProjectsList /> */}
+      <ProjectsList />
       <ServicesList />
       <ClockContactsSection />
     </div>
