63f8619d91094d844b9e1161f66713bc4270ef43
Serhii Kushnir <s1983kushnir@gmail.com>
2025-02-03 20:39:48 +0100

    Hero comited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { ProjectsList } from "./components/ProjectsList";
-import { Hero } from "./components/Hero";
+// import { Hero } from "./components/Hero";
 import { ServicesList } from "./components/ServicesList";
 import { ClockContactsSection } from "./components/ClockContactSection";
 
@@ -7,7 +7,7 @@ import { ClockContactsSection } from "./components/ClockContactSection";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between gap-24 lg:gap-[144px] pt-16 pb-36">
-      <Hero />
+      {/* <Hero /> */}
       <ProjectsList />
       <ServicesList />
       <ClockContactsSection />
