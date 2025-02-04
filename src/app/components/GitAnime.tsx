82a098fd0f1c3ef11dce683e3291568bf4fddf70
Serhii Kushnir <s1983kushnir@gmail.com>
2025-02-04 08:25:49 +0100

    Comited Lotti

--- src/app/components/GitAnime.tsx
+++ src/app/components/GitAnime.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Lottie from "lottie-react";
-import githubAnime from "../../../public/animation/github2.json"
+// import Lottie from "lottie-react";
+// import githubAnime from "../../../public/animation/github2.json"
 
 export const GitAnime = () => {
 
 if (typeof document === undefined) {
   return <div>Loading</div>;
 }
-  return <Lottie animationData={githubAnime} loop={false} autoplay={true} width={"100px"} height={"100px"} />;};
+  // return <Lottie animationData={githubAnime} loop={false} autoplay={true} width={"100px"} height={"100px"} />;
+};
