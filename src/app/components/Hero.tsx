d544f750c2ae8a9d427519fcc2eb5e20ef29be36
Serhii Kushnir <s1983kushnir@gmail.com>
2025-02-04 08:42:10 +0100

    Hero ssr

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 // import { lazy } from "react";
 import { Button } from "./ui/Button";
 import dynamic from "next/dynamic";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { ProjectsList } from "./components/ProjectsList";
-// import { Hero } from "./components/Hero";
+import { Hero } from "./components/Hero";
 import { ServicesList } from "./components/ServicesList";
 import { ClockContactsSection } from "./components/ClockContactSection";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const HeroDynamic = dynamic(
-  () =>
-    import("./components/Hero").then((module) => ({ default: module.Hero }))
-);
+// const HeroDynamic = dynamic(
+//   () =>
+//     import("./components/Hero").then((module) => ({ default: module.Hero }))
+// );
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between gap-24 lg:gap-[144px] pt-16 pb-36">
-      <HeroDynamic />
+      <Hero />
       <ProjectsList />
       <ServicesList />
       <ClockContactsSection />
