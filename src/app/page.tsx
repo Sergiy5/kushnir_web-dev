969d8571fb96557a2a6ea473dc348863f0f22cca
Serhii Kushnir <s1983kushnir@gmail.com>
2025-02-04 22:14:24 +0100

    Uncommited clock section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Hero />
       <ProjectsList />
       <ServicesList />
-      {/* <ClockContactsSection /> */}
+      <ClockContactsSection />
     </div>
   );
 }
