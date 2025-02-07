5e885826c6b29dcdb790817cdbf18c512bcf2582
Serhii Kushnir <s1983kushnir@gmail.com>
2025-02-07 18:42:52 +0100

    Chang font size to 5xl for 404  title

--- src/app/components/NotFound.tsx
+++ src/app/components/NotFound.tsx
@@ -19,7 +19,7 @@ export const NotFound: React.FC = () => {
   return (
     <div className="container flex flex-col justify-center items-start w-full gap-[100px] pt-10 pb-96">
       <div className="flex flex-col justify-start items-start  flex-shrink-0 relative gap-6">
-        <h2 className=" font-bold text-left text-black">
+        <h2 className=" font-bold text-5xl text-left text-black">
           The page wasn’t found, let’s go to a better place.
         </h2>
         <div className="flex justify-center items-center gap-2.5 py-1 border-t-0 border-r-0 border-b border-black">
