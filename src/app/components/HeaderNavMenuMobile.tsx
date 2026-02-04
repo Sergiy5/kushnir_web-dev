d10742222be41e2696963b3dd23f61e01e612f95
Serhii Kushnir <s1983kushnir@gmail.com>
2026-02-04 19:23:39 +0100

    Fixed images setting for git hub pages and acnchors to sections

--- src/app/components/HeaderNavMenuMobile.tsx
+++ src/app/components/HeaderNavMenuMobile.tsx
@@ -47,7 +47,7 @@ export const HeaderNavMenuMobile: React.FC<HeaderNavMenuMobileProps> = ({
       </NavItem>
 
       <LinkBtn
-        href="cv/Serhii_Kushnir_Full_Stack_Developer.pdf"
+        href="cv/serhii_kushnir_full_stack_developer.pdf"
         ariaLabel="Download CV"
         clasName="downloadLinkBtn w-full flex"
       >
